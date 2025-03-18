d40bc645b787b340b382d37658578056ddd7a18f
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-03-17 17:47:42 -0700

    initial commit. skeleton layout.

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,4 @@
-const initialTodos = [
-  {
-    id: "7cec7373-681b-49d9-b065-021d61a69d03",
-    name: "Read the sprint's theory",
-    completed: true,
-    date: new Date(),
-  },
-  {
-    id: "a7bfd5ef-37cc-4fa6-89f2-cac098a8aeba",
-    name: "Read project instructions",
-    completed: false,
-    date: new Date(),
-  },
-  {
-    id: "aa486839-63ab-437f-b8a2-29ab217dff4f",
-    name: "Complete project",
-    completed: false,
-    date: new Date(),
-  },
-];
-
-const validationConfig = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  errorClass: "popup__error_visible",
-  inputErrorClass: "popup__input_type_error",
-  inactiveButtonClass: "button_disabled",
-};
+import { initialTodos, validationConfig } from "../utils/constants.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -43,7 +15,9 @@ const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
 
+//---------------------------------------------------------------------
 // The logic in this function should all be handled in the Todo class.
+
 const generateTodo = (data) => {
   const todoElement = todoTemplate.content
     .querySelector(".todo")
@@ -56,6 +30,8 @@ const generateTodo = (data) => {
 
   todoNameEl.textContent = data.name;
   todoCheckboxEl.checked = data.completed;
+
+  //--------------------------------------------------------------------
 
   // Apply id and for attributes.
   // The id will initially be undefined for new todos.
